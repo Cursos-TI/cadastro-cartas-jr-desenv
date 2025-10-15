b8d0895edbe29db0d37c978a271700ebaa4cd7c4
jr-desenv <31509776+jr-desenv@users.noreply.github.com>
2025-10-15 22:43:28 +0000

    Definição das variáveis do projeto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,24 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // Declaracao de variáveis para carta 1
+  char estado1;
+  char codigo1[4];
+  char cidade1[50];
+  int  populacao1;
+  float area1;
+  float pib1;
+  int pontos_turisticos1;
+
+  // Declaracao de variáveis para carta 2
+  char estado2;
+  char codigo2[4];
+  char cidade2[50];
+  int  populacao2;
+  float area2;
+  float pib2;
+  int pontos_turisticos2;
+
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
