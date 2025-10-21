3e29bed18101c8d98f11fe1dbde75367669ca38f
jr-desenv <31509776+jr-desenv@users.noreply.github.com>
2025-10-21 15:59:18 +0000

    Declaracao de Variaveis

---
+++ CartasSuperTrunfo.c
@@ -0,0 +1,17 @@
+    // Declaração das variáveis para a Carta 1
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos_turisticos1;
+
+    // Declaração das variáveis para a Carta 2
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos_turisticos2;
